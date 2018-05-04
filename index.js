09a883b04a7d465ebec47413f08d6bcb6157a41a
Richard Spitkovsky <rjspitkovsky@gmail.com>
2018-05-04 00:42:23 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -10,11 +10,13 @@ function logDriversByHometown(drivers, location) {
 }
 
 function driversByRevenue(drivers) {
-
+  drivers.sort(function(a,b) {
+    return a.revenue - b.revenue
+  })
 }
 
 function driversByName(drivers) {
-  
+
 }
 
 
